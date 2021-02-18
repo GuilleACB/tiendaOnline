b2358acd8f3d7da56f3051850596d66c1e11263c
DESKTOP-QFEO4LF\Prometeus <alexandrocb2793@gmail.com>
2021-02-18 23:19:49 +0100

    Introduccion de texto aviso legal

--- src/app/web/pages/aviso-legal/aviso-legal.component.ts
+++ src/app/web/pages/aviso-legal/aviso-legal.component.ts
@@ -6,7 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aviso-legal.component.scss']
 })
 export class AvisoLegalComponent implements OnInit {
-
+  denominacionSocial: string = 'rellenarEmpresa';
+  nombreComercial: string = 'rellenarEmpresa';
+  domicilio: string = 'rellenarEmpresa';
+  cif: string = 'rellenarEmpresa';
+  emailEmpresa: string = 'rellenarEmpresa';
+  telefono: string = 'rellenarEmpresa';
+  sitioWeb: string = 'url_Web';
+  registroMercantil: string = 'RellenarEmpresa';
+  tomoMercantil: string = 'RellenarEmpresa';
+  libroMercantil: string = 'RellenarEmpresa';
+  folioMercantil: string = 'RellenarEmpresa';
+  hojaMercantil: string = 'RellenarEmpresa';
+  inscripcionMercantil: string = 'RellenarEmpresa';
+  nombreEmpresa: string = 'nombreEmpresa';
+  ciudadTribunal: string = 'ciudadTribunal';
   constructor() { }
 
   ngOnInit(): void {
