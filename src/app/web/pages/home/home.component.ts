df9fe77bff26eeb1d90c5145b397f53c9e4c8157
guillermogcb@gmail.com <guilleacb@gmail.com>
2021-04-15 16:46:13 +0200

    retoques en la vista home

--- src/app/web/pages/home/home.component.ts
+++ src/app/web/pages/home/home.component.ts
@@ -16,6 +16,11 @@ export class HomeComponent implements OnInit {
                          cristal y paneles de cristal a medida. También somos fabricantes e instaladores
                          de puertas y ventanas de cristal, mamparas de ducha y espejos para 
                          viviendas particulares.`;
+
+  colaborador_logo: string = '../../assets/img/Ocaso_logo.JPG';
+  colaborador_logo_des: string = 'Descripción colaboración';     
+  colaborador_2_logo: string = '../../assets/img/SantaLucia_logo.JPG';
+  colaborador_2_logo_des: string = 'Descripción colaboración';                   
                          
   constructor() { }
 
