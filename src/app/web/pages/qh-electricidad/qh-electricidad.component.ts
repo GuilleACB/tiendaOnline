d86feb5ecefc4241b9e64ff97e81fb33e2d24770
DESKTOP-QFEO4LF\Prometeus <alexandrocb2793@gmail.com>
2021-05-23 20:26:01 +0200

    Finalizacion pagina-home

---
+++ src/app/web/pages/qh-electricidad/qh-electricidad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qh-electricidad',
+  templateUrl: './qh-electricidad.component.html',
+  styleUrls: ['./qh-electricidad.component.scss']
+})
+export class QhElectricidadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
